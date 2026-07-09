7406bef05e44b3e02d1dfaa04f3de3d43c458640
sujan-2095 <sujanpalanisamy20@gmail.com>
2026-07-09 14:34:58 +0530

    Week 4 Assessment

---
+++ Week 4/Creating users and roles in Spring Security/springlearn/src/main/java/com/cognizant/springlearn/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.cognizant.springlearn.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/authenticate").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
